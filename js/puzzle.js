9db25a3ba7014eb4ca5ccdd357a96e3b32d1ae7c
Steven Weng <bee0060@126.com>
2013-06-12 00:38:05 +0800

    daily commit
    remove the not neccessary thing. implete in the half

--- js/puzzle.js
+++ js/puzzle.js
@@ -117,7 +117,89 @@ function showDifficultLevel()
 }
 
 function cutImg(){
-	var radSelectedMode = $('#ulModeList li input[type=radio][name=mode]:checked');
-	alert(radSelectedMode.length);
+	//var radSelectedMode = $('#ulModeList li input[type=radio][name=mode]:checked');
+	//alert(radSelectedMode.length);
+	
+	var imgWidth = imgOrigin.width,
+		imgHeight = imgOrigin.height,
+		hozCount = 4, verCount = 4, 
+		cellWidth = parseInt(imgWidth/hozCount),
+		lastCellWidth = imgWidth- cellWidth*hozCount;
+		cellHeight = parseInt(imgHeight/verCount),
+		lastCellHeight = imgHeight-cellHeight*verCount;
+	
+	hozCount += (lastCellWidth > 0 ? 1 : 0);
+	verCount += (lastCellHeight > 0 ? 1 : 0);
+	
+	lastCellWidth = cellWidth;
+	lastCellHeight = cellHeight;
+	
+	$('#divPlayField').remove();
+	var playField = document.createElement("div");
+	playField.id="divPlayField";
+	playField.style.cssText = "border:1px solid #000;height:"+imgHeight+"px;width:"+imgWidth+"px;";
+	
+	var cells = [],
+		mixedCells,
+		ul = document.createElement("ul"),
+		li,
+		currentCellWidth, currentCellHeight,
+		currentPosition;
+		
+	for(var y=0; y<verCount; y++)
+	{
+		for(var x=0; x<hozCount; x++)
+		{			
+			li = document.createElement("li");
+						
+			currentCellWidth = (x==hozCount-1 ? lastCellWidth : cellWidth);
+			currentCellHeight = (y==verCount-1 ? lastCellHeight : cellHeight);
+			currentPosition = '-'+x*currentCellWidth+'px -'+y*currentCellHeight+'px'
+			
+			$(li).css({
+				'background-image':"url("+imgOrigin.src+")",
+				'background-position':currentPosition,
+				'display':'block',
+				'float':'left',
+				'height':currentCellHeight+"px",
+				'overflow':'hidden',
+				'width':currentCellWidth+"px"});
+			
+			cells.push(li);
+		}		
+	}
+	
+	mixedCells = mixUpCells(cells);
+	alert(mixedCells.length);
+	fillUl(ul, mixedCells);
+alert(mixedCells.length);
+	playField.appendChild(ul);
+	$('.content').append(playField);
 }
 
+function mixUpCells(cells)
+{
+	var cellCount = cells.length,
+		seed = cellCount,
+		randomNum,
+		pointInt,
+		pointCell,
+		outputCells =[];
+		 
+	while(seed>0){
+		pointInt = Math.round(Math.random()*seed-0.5);
+		pointCell = cells.splice(pointInt,1);
+		outputCells.push(pointCell);
+		
+		seed--;
+	} 
+	return outputCells;
+}
+
+function fillUl(ul, liArray)
+{
+	for( var i =0;i<liArray.length;i++)
+	{
+		ul.appendChild(liArray[i]);
+	}	
+}
